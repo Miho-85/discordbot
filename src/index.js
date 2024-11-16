c2c40eabee0560c0fbf3fb483b8aada3ed21786d
Miho8115 <mihovpn@gmail.com>
2024-11-16 19:19:00 +0100

    Slash commands babyyyy

--- src/index.js
+++ src/index.js
@@ -16,6 +16,20 @@ client.on('ready', (c) => {
     console.log(`${client.user.displayName} is online | [NO | F811K5]`)
 });
 
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'hey') {
+      return interaction.reply('hey!');
+    }
+  
+    if (interaction.commandName === 'ping') {
+      return interaction.reply('Pong!');
+    }
+  });
+
+
+
 client.on('messageCreate', (message) => {
     console.log(`${message.author.displayName} : ${message.content}`)
 });
